8a6fa387339d031f2333de61b341cc43e7132813
QSP <QSP@Desktop-Monitor-1st-Floor-Room3>
2024-10-28 10:25:10 +0530

    Merge branch 'master' of https://github.com/Bindu888git/BDD.git

--- BDD/src/main/java/runner/Runner.java
+++ BDD/src/main/java/runner/Runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
-		features="featurefiles/LearnHooks.feature",
+		features="featurefiles",
 		glue={"stepdefinitions","hooks"},
 		dryRun = false,
 		monochrome = true,
